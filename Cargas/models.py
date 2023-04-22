7fd228b9a1e27afb95bdb25db280cca9ae91298a
JuanesFosch <juan.fosch@gmail.com>
2023-04-22 11:01:10 -0300

    Actualización

--- Cargas/models.py
+++ Cargas/models.py
@@ -5,19 +5,19 @@ from django.db import models
 
 class Pacientes(models.Model):
     Nombre= models.CharField(max_length=40)
-    DNI= models.IntegerField(primary_key=True)
+    DNI= models.IntegerField(primary_key=True, default=0000)
     Teléfono = models.IntegerField()
     E_mail= models.CharField(max_length=40)
     Obra_Social_Prepaga= models.CharField(max_length=40)    # Esto se debería poder elegir de una lista.
 
-    def __str__(self):
-        return self.Nombre
+    class Meta:
+        verbose_name_plural= "Pacientes"
 
 
 class Presupuestos(models.Model):
     #consultar Nombre
     DNI= models.ForeignKey("Pacientes",on_delete=models.CASCADE)
-    Número_de_orden= models.IntegerField(primary_key=True,auto_created=True)  # número de orden automático
+    Número_de_orden= models.IntegerField(primary_key=True,auto_created=True,default=0000)  # número de orden automático
     Fecha= models.DateField(auto_now_add=True,blank=True)
     # Tratamiento consultado a la lista de códigos
     Tratamiento_1= models.CharField(max_length=100,blank=True)  # Para tratamientos por particular
@@ -27,8 +27,8 @@ class Presupuestos(models.Model):
     #Forma de pago
     #Cuotas
 
-    def __str__(self):
-        return "Número de orden: " + str(self.Número_de_orden) 
+    class Meta:
+        verbose_name_plural= "Presupuestos" 
 
 class Cobranzas(models.Model):
     pass
